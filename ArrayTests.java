921f74954a2070a35108eac566e2113cd98dde2f
nicholas-ngyn <122708113+nicholas-ngyn@users.noreply.github.com>
2023-04-20 16:43:52 -0700

    Lab3

--- ArrayTests.java
+++ ArrayTests.java
@@ -5,14 +5,20 @@ public class ArrayTests {
 	@Test 
 	public void testReverseInPlace() {
     int[] input1 = { 3 };
+    int[] input2 = {1, 2, 3, 4, 5};
     ArrayExamples.reverseInPlace(input1);
+    ArrayExamples.reverseInPlace(input2);
     assertArrayEquals(new int[]{ 3 }, input1);
+    assertArrayEquals(new int[]{5,4,3,2,1}, input2);
 	}
 
 
   @Test
   public void testReversed() {
     int[] input1 = { };
+    int[] input2 = {10, 100, 1000};
+    ArrayExamples.reversed(input2);
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+    assertArrayEquals(new int[]{1000,100,10}, input2);
   }
 }
